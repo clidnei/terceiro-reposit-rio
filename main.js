75058e6e2b05f613da4bb51ab8b0f01bb7d4694c
clidnei <108068281+clidnei@users.noreply.github.com>
2024-04-09 22:50:49 -0300

    19/04

--- main.js
+++ main.js
@@ -21,4 +21,23 @@ contadores[0].textContent = "contagem regressiva";
 const tempoObjetivol = new Date("2024-05-10T23:59:59");
 let tempoAtual = new Date();
 
+contadores[0].textContent = calcularTempo(tempoObjetivo1);
+function calculaTempo (tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo -  tempoAtual;
+
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(horas/24);
+
+    segundos %=60;
+    minutos %=60;
+    horas %=24;
+
+    return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
+
+
+
+}
+
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
